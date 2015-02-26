0d3eb76db653a1deb7a1e1b836612db803f97fa2
Christian Richter <kristian.richter@googlemail.com>
2015-02-27 00:48:35 +0100

    Added CSS file to unclutter XING. Intended for use in Stylish browser extension.

--- javascript/linkedin_to_xing.js
+++ javascript/linkedin_to_xing.js
@@ -4,4 +4,3 @@ var xing_search_string = 'https://www.xing.com/search/members?advanced_form=&q[k
 window.open(xing_search_string)
 // Otherwise the source website will be changed to the default value of last operation.
 void(0)
-
