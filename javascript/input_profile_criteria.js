bbd5ec133e07c1b223a651ac51096ddb1853df55
Christian Richter <judge.spambox+github@googlemail.com>
2015-05-06 15:31:15 +0200

    Create input_profile_criteria.js

---
+++ javascript/input_profile_criteria.js
@@ -0,0 +1,23 @@
+var insertData = sessionStorage.insData;
+if (insertData === undefined) {
+  var promptData = prompt('No profile criteria found for this site. Please enter data');
+  sessionStorage.insData = promptData;
+  insertData = promptData;
+}
+
+if (window.location.href.indexOf("xing.com") > -1) {
+  var oldInnerHTML = document.getElementsByClassName('grey')[0].innerHTML;
+  document.getElementsByClassName('grey')[0].innerHTML = oldInnerHTML + insertData;
+} else if (window.location.href.indexOf("experteer.de") > -1) {
+  var oldInnerHTML = document.getElementsByClassName('tab-content-notes')[0].innerHTML;
+  document.getElementsByClassName('tab-content-notes')[0].innerHTML = oldInnerHTML + insertData;
+}
+else if (window.location.href.indexOf("linkedin.com/pub") > -1 || window.location.href.indexOf("linkedin.com/profile") > -1 ) {
+  var oldInnerHTML = document.getElementsByClassName('profile-overview-content')[0].innerHTML;
+  document.getElementsByClassName('profile-overview-content')[0].innerHTML = oldInnerHTML + insertData;
+}
+else if (window.location.href.indexOf("linkedin.com/recruiter") > -1) {
+  var oldInnerHTML = document.getElementsByClassName('info-container')[0].innerHTML;
+  document.getElementsByClassName('info-container')[0].innerHTML = oldInnerHTML + insertData;
+}
+window.print();
